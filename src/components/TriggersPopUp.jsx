7cbd282dd5abb55d17558dee9a1f5101ba3173e4
wwwsapir <wwwsapir@gmail.com>
2020-05-08 19:31:01 +0300

    TriggersPopUp: added no triggers message

--- src/components/TriggersPopUp.jsx
+++ src/components/TriggersPopUp.jsx
@@ -7,19 +7,29 @@ const TriggersPopUp = (props) => {
     <div className="triggers-popup">
       <div className="menu flex-column">
         <div dangerouslySetInnerHTML={{ __html: '"' + props.html + '"' }}></div>
-        <div className="menu-header mb-4 mt-4">Assigned Triggers:</div>
-        {props.triggers.map((trigger, i) => (
-          <div key={i} className="row">
-            <div className="col-sm-3">{trigger.name + ":"}</div>
-            <div className="col-sm-3">{trigger.type + ":"}</div>
-            <div className="col-sm-3">{trigger.valueName + ":"}</div>
-            <div className="col-sm-3">
-              <button className="btn btn-danger">Remove This Trigger</button>
-            </div>
+        <div className="menu-header mb-3 mt-4">Assigned Triggers:</div>
+        {props.triggers.length > 0 ? (
+          <div className="row mb-4">
+            {props.triggers.map((trigger, i) => (
+              <div key={i}>
+                <div className="col-sm-3">{trigger.name + ":"}</div>
+                <div className="col-sm-3">{trigger.type + ":"}</div>
+                <div className="col-sm-3">{trigger.valueName + ":"}</div>
+                <div className="col-sm-3">
+                  <button className="btn btn-danger">
+                    Remove This Trigger
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        ) : (
+          <div className="row mb-4">
+            <div className="col ml-3">There are currently no assigned triggers.</div>
+          </div>
+        )}
         <div className="row">
-          <button onClick={props.closePopUp} className="btn btn-light col">
+          <button onClick={props.closePopUp} className="btn btn-light col-4 offset-4">
             Close
           </button>
         </div>
